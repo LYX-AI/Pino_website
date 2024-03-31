663fc10a6ab0daebaccd6b2f5ef1238cbd7e819f
lyx_3 <lyx_316190121@163.com>
2024-03-31 15:27:26 +0800

    the forth day

--- pino_website/urls.py
+++ pino_website/urls.py
@@ -22,4 +22,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
 ]
+
--- pino_website/views.py
+++ pino_website/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 def index(request):
+    return render(request, 'index.html')
     # 这使用模板加载器 loader 来获取 index.html 模板。它基本上是在告诉 Django 去项目的模板目录下查找名为 index.html 的文件
-    template=loader.get_template('index.html')
-    return HttpResponse(template.render({},request))
+    # template=loader.get_template('index.html')
+    # return HttpResponse(template.render({},request))
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
