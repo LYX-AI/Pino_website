acd17acc17ee437b9724aaa0d835992e50eceda4
LYX-AI <lyx_316190121@163.com>
2025-02-04 00:37:02 +0800

    Added new migrations

--- pino_website/apps/accounts/admin.py
+++ pino_website/apps/accounts/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin#导入 Django 自带的 admin 模块，用于管理后台界面。
-from .models import UserPersona, UserProfile
+from .models import UserPersona, UserProfile,UserInterest
 
 admin.site.register(UserPersona)
 admin.site.register(UserProfile)
+admin.site.register(UserInterest)
